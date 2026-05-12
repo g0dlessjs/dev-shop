5b92fdd09b226f73e1dc0314261252cef4f75db7
g0dlessjs <juangalarce.dev@gmail.com>
2026-05-12 17:05:11 -0400

    feat: implementar página de categoría dinámica y manejo de error 404

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,7 +1,21 @@
-export default function () {
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function ({ params }: Props) {
+  const { id } = await params;
+
+  if (id === "kids") {
+    notFound();
+  }
+
   return (
     <div>
-      <h1>Category Page</h1>
+      <h1>Categoria {id}</h1>
     </div>
   );
 }
