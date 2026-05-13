d75cc299f5fcf8efb3e0e4bd96adbb9a3f69248d
g0dlessjs <juangalarce.dev@gmail.com>
2026-05-12 20:11:33 -0400

    feat(products): implementar componente ProductGridItem y refactorizar visualización del grid

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -6,7 +6,7 @@ export default function ShopLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen bg-linear-to-b from-gray-200 via-white to-gray-200">
       <TopMenu />
       <div className="px-4 sm:px-10">{children}</div>
     </main>
